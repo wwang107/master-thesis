ad95efaa0223e878d90a2cc2fda65c1d1c1402eb
wei <wei@pop-os.localdomain>
2020-10-16 17:52:23 +0200

    Formatting stuff

--- src/data/COCOKeypoints.py
+++ src/data/COCOKeypoints.py
@@ -8,17 +8,18 @@ import torch
 import pycocotools
 from .COCODataset import CocoDataset
 
+
 class CocoKeypoints(CocoDataset):
-    def __init__(self,
-                 cfg,
-                 dataset_name,
-                 remove_images_without_annotations,
-                 direction_keypoints_generator=None,
-                 heatmap_generator=None,
-                 transforms=None):
-        super().__init__(cfg.DATASET.COCOROOT,
-                         dataset_name,
-                         cfg.DATASET.DATA_FORMAT)
+    def __init__(
+        self,
+        cfg,
+        dataset_name,
+        remove_images_without_annotations,
+        direction_keypoints_generator=None,
+        heatmap_generator=None,
+        transforms=None,
+    ):
+        super().__init__(cfg.DATASET.COCOROOT, dataset_name, cfg.DATASET.DATA_FORMAT)
 
         self.num_joints = cfg.DATASET.NUM_JOINTS
 
@@ -35,38 +36,40 @@ class CocoKeypoints(CocoDataset):
     def __getitem__(self, idx):
         img, anno = super().__getitem__(idx)
         mask = np.array(self.get_mask(anno, idx), dtype=np.uint8)
-        anno = [
-            obj for obj in anno
-            if obj['iscrowd'] == 0 or obj['num_keypoints'] > 0
-        ]
+        anno = [obj for obj in anno if obj["iscrowd"] == 0 or obj["num_keypoints"] > 0]
 
         joints = self.get_joints(anno)
 
         if self.transforms:
-            img, mask, joints = self.transforms(
-                img, mask, joints
-            )
-        dir_keypoints = self.direction_keypoints_generator(joints, 0.1)        
+            img, mask, joints = self.transforms(img, mask, joints)
+        dir_keypoints = self.direction_keypoints_generator(joints, 0.1)
         heatmap = self.heatmap_generator(
             np.concatenate((joints, dir_keypoints), axis=1)
-            )
+        )
 
-        return {'images': img, 'masks':mask, 'heatmaps': heatmap, 'joints': joints, 'directional_keypoints': dir_keypoints}
+        return {
+            "images": img,
+            "masks": mask,
+            "heatmaps": heatmap,
+            "joints": joints,
+            "directional_keypoints": dir_keypoints,
+        }
 
     def get_mask(self, anno, idx):
         coco = self.coco
         img_info = coco.loadImgs(self.ids[idx])[0]
 
-        m = np.zeros((img_info['height'], img_info['width']))
+        m = np.zeros((img_info["height"], img_info["width"]))
 
         for obj in anno:
             # if obj['iscrowd']:
             #     rle = pycocotools.mask.frPyObjects(
             #         obj['segmentation'], img_info['height'], img_info['width'])
             #     m += pycocotools.mask.decode(rle)
-            if obj['num_keypoints'] > 0:
+            if obj["num_keypoints"] > 0:
                 rles = pycocotools.mask.frPyObjects(
-                    obj['segmentation'], img_info['height'], img_info['width'])
+                    obj["segmentation"], img_info["height"], img_info["width"]
+                )
                 for rle in rles:
                     m += pycocotools.mask.decode(rle)
 
@@ -78,7 +81,8 @@ class CocoKeypoints(CocoDataset):
         joints = np.zeros((num_people, self.num_joints, 3))
 
         for i, obj in enumerate(anno):
-            joints[i, :self.num_joints, :3] = \
-                np.array(obj['keypoints']).reshape([-1, 3])
+            joints[i, : self.num_joints, :3] = np.array(obj["keypoints"]).reshape(
+                [-1, 3]
+            )
 
         return joints
--- src/data/PanopticDataset.py
+++ src/data/PanopticDataset.py
@@ -4,7 +4,6 @@ from typing import Sequence
 import torch
 import cv2
 import numpy as np
-from torch import zero_
 from data import panutils
 from pathlib import Path
 from torch.utils.data import Dataset
@@ -28,7 +27,7 @@ CMU_TO_COCO_JOINT_LABEL = {
     15: 2,  # eye_r
     16: 1,  # eye_l
     17: 4,  # ear_r
-    18: 3  # ear_l
+    18: 3,  # ear_l
 }
 """
 Panoptic Joint Label:
@@ -54,18 +53,34 @@ Panoptic Joint Label:
 """
 WIDTH = 1920
 HEIGHT = 1080
-TRAINING_CAMERA_ID = [(0,i) for i in range(0,31) if i != 21] # missing view 21
-VALIDATION_CAMERA_ID = [(0, 12), (0, 6), (0, 13), (0, 3),(0,1)]
-HD_IMG = 'hdImgs'
-BODY_EDGES = np.array([[1, 2], [1, 4], [4, 5], [5, 6], [1, 3], [3, 7], [7, 8], [
-    8, 9], [3, 13], [13, 14], [14, 15], [1, 10], [10, 11], [11, 12]])-1
+TRAINING_CAMERA_ID = [(0, i) for i in range(0, 31) if i != 21]  # missing view 21
+VALIDATION_CAMERA_ID = [(0, 12), (0, 6), (0, 13), (0, 3), (0, 1)]
+HD_IMG = "hdImgs"
+BODY_EDGES = (
+    np.array(
+        [
+            [1, 2],
+            [1, 4],
+            [4, 5],
+            [5, 6],
+            [1, 3],
+            [3, 7],
+            [7, 8],
+            [8, 9],
+            [3, 13],
+            [13, 14],
+            [14, 15],
+            [1, 10],
+            [10, 11],
+            [11, 12],
+        ]
+    )
+    - 1
+)
 
-TRAIN_LIST = [
-    '160224_haggling1',
-    '170407_haggling_a1'
-]
+TRAIN_LIST = ["160224_haggling1", "170407_haggling_a1"]
 
-VAL_LIST = ['160226_haggling1']
+VAL_LIST = ["160226_haggling1"]
 
 
 class PanopticDataset(Dataset):
@@ -73,10 +88,9 @@ class PanopticDataset(Dataset):
     A dataset for CMU Panoptic
     """
 
-    def __init__(self, cfg,
-                 heatmap_generator=None,
-                 keypoint_generator=None,
-                 is_train=True):
+    def __init__(
+        self, cfg, heatmap_generator=None, keypoint_generator=None, is_train=True
+    ):
         super().__init__()
         self.cfg = cfg
         self.is_train = is_train
@@ -91,55 +105,59 @@ class PanopticDataset(Dataset):
         self.num_directional_keypoint = 55
 
         self.files = {}
-        self.files['sub_seq_data'] = []
+        self.files["sub_seq_data"] = []
         rootDir = Path(cfg.DATASET.CMUROOT)
-        self.seq_names = TRAIN_LIST if is_train == True else VAL_LIST
+        self.seq_names = TRAIN_LIST if is_train else VAL_LIST
         self.seq_names = sorted(self.seq_names)
 
         for seq_name in self.seq_names:
-            self.files[seq_name] = {
-                'cams_matrix': {}}
-            with open(rootDir.joinpath(seq_name, 'calibration_{0}.json'.format(seq_name))) as cfile:
+            self.files[seq_name] = {"cams_matrix": {}}
+            with open(
+                rootDir.joinpath(seq_name, "calibration_{0}.json".format(seq_name))
+            ) as cfile:
                 calib = json.load(cfile)
             # Cameras are identified by a tuple of (panel#,node#)
-            cameras = {(cam['panel'], cam['node'])                       : cam for cam in calib['cameras']}
+            cameras = {(cam["panel"], cam["node"]): cam for cam in calib["cameras"]}
             # Select an HD camera (0,0) - (0,30), where the zero in the first index means HD camera
-            print('Loading hd images path...')
+            print("Loading hd images path...")
             for cam_id in TRAINING_CAMERA_ID:
                 if cam_id in cameras:
                     cam = cameras[cam_id]
-                    cam['K'] = np.matrix(cam['K'])
-                    cam['distCoef'] = np.array(cam['distCoef'])
-                    cam['R'] = np.matrix(cam['R'])
-                    cam['t'] = np.array(cam['t']).reshape((3, 1))
-                    self.files[seq_name]['cams_matrix'][cam_id] = cam
+                    cam["K"] = np.matrix(cam["K"])
+                    cam["distCoef"] = np.array(cam["distCoef"])
+                    cam["R"] = np.matrix(cam["R"])
+                    cam["t"] = np.array(cam["t"]).reshape((3, 1))
+                    self.files[seq_name]["cams_matrix"][cam_id] = cam
 
-            
             # Load skeleton
-            skel_json_paths = sorted(list(rootDir.joinpath(
-                seq_name, 'hdPose3d_stage1_coco19').glob('*.json')))
-            
-            start_frame = self.num_frames_in_subseq if is_train else len(skel_json_paths)//2
+            skel_json_paths = sorted(
+                list(
+                    rootDir.joinpath(seq_name, "hdPose3d_stage1_coco19").glob("*.json")
+                )
+            )
+
+            start_frame = (
+                self.num_frames_in_subseq if is_train else len(skel_json_paths) // 2
+            )
             end_frame = len(skel_json_paths) if is_train else start_frame + 200
             step = self.num_frames_in_subseq if is_train else 1
-            print('Loading skeleton...')
+            print("Loading skeleton...")
             for i in tqdm(range(start_frame, end_frame, step), desc=seq_name):
                 pose3d_subseq = []
                 hd_img_subseq = {}
-                subseq_data = {'seq_name':None, '3d_pose':None, 'hd_img':{}}
+                subseq_data = {"seq_name": None, "3d_pose": None, "hd_img": {}}
                 for j in range(self.num_frames_in_subseq, 0, -1):
-                    with open(skel_json_paths[i-j]) as sfile:
-                        bodies = json.load(sfile)['bodies']
+                    with open(skel_json_paths[i - j]) as sfile:
+                        bodies = json.load(sfile)["bodies"]
                     if len(bodies) == 0:
                         break
 
                     poses3d = []
                     for body in bodies:
-                        pose3d = np.array(body['joints19']).reshape(
-                            (-1, 4)).transpose()
+                        pose3d = np.array(body["joints19"]).reshape((-1, 4)).transpose()
                         joints_vis = pose3d[-1, :] > 0.1
                         if not joints_vis[self.root_id]:
-                                continue
+                            continue
                         poses3d.append(pose3d)
                     if len(poses3d) == 0:
                         break
@@ -148,98 +166,131 @@ class PanopticDataset(Dataset):
                     for cam_id in TRAINING_CAMERA_ID:
                         if cam_id not in hd_img_subseq:
                             hd_img_subseq[cam_id] = []
-                        postfix = skel_json_paths[i-j].name.replace('body3DScene', '')
-                        prefix = '{:02d}_{:02d}'.format(cam_id[0], cam_id[1])
+                        postfix = skel_json_paths[i - j].name.replace("body3DScene", "")
+                        prefix = "{:02d}_{:02d}".format(cam_id[0], cam_id[1])
                         image = rootDir.joinpath(
-                            seq_name, 'hdImgs', prefix, prefix + postfix)
-                        image = Path(str(image).replace('json', 'jpg'))
+                            seq_name, "hdImgs", prefix, prefix + postfix
+                        )
+                        image = Path(str(image).replace("json", "jpg"))
                         hd_img_subseq[cam_id].append(image)
 
                 if len(pose3d_subseq) != self.num_frames_in_subseq:
                     continue
                 else:
-                    subseq_data['seq_name'] = seq_name
-                    subseq_data['3d_pose'] = pose3d_subseq
-                    subseq_data['hd_img'] = hd_img_subseq
-                    self.files['sub_seq_data'].append(subseq_data)
-            
-    def __len__(self):
-        return len(self.files['sub_seq_data'])
-    
-    def __getitem__(self, idx):
-        subseq_pose3d = self.files['sub_seq_data'][idx]['3d_pose']
-        subseq_hdImg = self.files['sub_seq_data'][idx]['hd_img']
-        seq_name = self.files['sub_seq_data'][idx]['seq_name']
-        camera_ids = random.sample(TRAINING_CAMERA_ID, self.num_view) if self.is_train else VALIDATION_CAMERA_ID
+                    subseq_data["seq_name"] = seq_name
+                    subseq_data["3d_pose"] = pose3d_subseq
+                    subseq_data["hd_img"] = hd_img_subseq
+                    self.files["sub_seq_data"].append(subseq_data)
 
-        hm = np.zeros((
-            self.num_directional_keypoint,
-            self.cfg.DATASET.OUTPUT_SIZE[1], self.cfg.DATASET.OUTPUT_SIZE[0],
-            self.num_view,
-            self.num_frames_in_subseq), dtype=np.float32)
-        img = np.zeros((
-            self.cfg.DATASET.INPUT_SIZE, self.cfg.DATASET.INPUT_SIZE,
-            3,
-            self.num_view,
-            self.num_frames_in_subseq), np.uint8)
-        pose2d = np.zeros((
-            self.num_max_people,
-            3,
-            self.num_joints,
-            self.num_view,
-            self.num_frames_in_subseq), np.float32)
-        keypoint2d = np.zeros((
-            self.num_max_people,
-            self.num_directional_keypoint,
-            3,
-            self.num_view,
-            self.num_frames_in_subseq), np.float32)
+    def __len__(self):
+        return len(self.files["sub_seq_data"])
+
+    def __getitem__(self, idx):
+        subseq_pose3d = self.files["sub_seq_data"][idx]["3d_pose"]
+        subseq_hdImg = self.files["sub_seq_data"][idx]["hd_img"]
+        seq_name = self.files["sub_seq_data"][idx]["seq_name"]
+        camera_ids = (
+            random.sample(TRAINING_CAMERA_ID, self.num_view)
+            if self.is_train
+            else VALIDATION_CAMERA_ID
+        )
+
+        hm = np.zeros(
+            (
+                self.num_directional_keypoint,
+                self.cfg.DATASET.OUTPUT_SIZE[1],
+                self.cfg.DATASET.OUTPUT_SIZE[0],
+                self.num_view,
+                self.num_frames_in_subseq,
+            ),
+            dtype=np.float32,
+        )
+        img = np.zeros(
+            (
+                self.cfg.DATASET.INPUT_SIZE,
+                self.cfg.DATASET.INPUT_SIZE,
+                3,
+                self.num_view,
+                self.num_frames_in_subseq,
+            ),
+            np.uint8,
+        )
+        pose2d = np.zeros(
+            (
+                self.num_max_people,
+                3,
+                self.num_joints,
+                self.num_view,
+                self.num_frames_in_subseq,
+            ),
+            np.float32,
+        )
+        keypoint2d = np.zeros(
+            (
+                self.num_max_people,
+                self.num_directional_keypoint,
+                3,
+                self.num_view,
+                self.num_frames_in_subseq,
+            ),
+            np.float32,
+        )
         num_person = np.zeros(self.num_frames_in_subseq, np.int)
-        s_x = self.cfg.DATASET.OUTPUT_SIZE[0]/self.cfg.DATASET.INPUT_SIZE
-        s_y = self.cfg.DATASET.OUTPUT_SIZE[1]/self.cfg.DATASET.INPUT_SIZE
+        s_x = self.cfg.DATASET.OUTPUT_SIZE[0] / self.cfg.DATASET.INPUT_SIZE
+        s_y = self.cfg.DATASET.OUTPUT_SIZE[1] / self.cfg.DATASET.INPUT_SIZE
         c = np.array([WIDTH / 2.0, HEIGHT / 2.0])
-        s = get_scale((WIDTH, HEIGHT),
-                      (self.cfg.DATASET.INPUT_SIZE, self.cfg.DATASET.INPUT_SIZE))
+        s = get_scale(
+            (WIDTH, HEIGHT), (self.cfg.DATASET.INPUT_SIZE, self.cfg.DATASET.INPUT_SIZE)
+        )
         r = 0
         trans = get_affine_transform(
-            c, s, r, (self.cfg.DATASET.INPUT_SIZE, self.cfg.DATASET.INPUT_SIZE))
+            c, s, r, (self.cfg.DATASET.INPUT_SIZE, self.cfg.DATASET.INPUT_SIZE)
+        )
 
         for f, frame3d in enumerate(subseq_pose3d):
             for k, cam_id in enumerate(camera_ids):
                 for p, pose3d in enumerate(frame3d):
                     pose3d = self.mapKeypointsToCOCO(pose3d)
                     pose2d[p, :, :, k, f] = self.map3DkeypointsTo2d(
-                        pose3d, self.files[seq_name]['cams_matrix'][cam_id])
+                        pose3d, self.files[seq_name]["cams_matrix"][cam_id]
+                    )
                     for i in range(self.num_joints):
                         pose2d[p, 0:2, i, k, f] = affine_transform(
-                            pose2d[p, 0:2, i, k, f], trans)
+                            pose2d[p, 0:2, i, k, f], trans
+                        )
                         pose2d[p, 0, i, k, f] = pose2d[p, 0, i, k, f] * s_x
                         pose2d[p, 1, i, k, f] = pose2d[p, 1, i, k, f] * s_y
-                num_person[f] = p+1
-                keypoint2d[0:num_person[f], 0:17, :, k, f] = pose2d[0:num_person[f],
-                                                                 :, 0:17, k, f].transpose((0, 2, 1))
-                keypoint2d[0:num_person[f], 17:, :, k, f] = \
-                    self.keypoint_generator(
-                        pose2d[0:num_person[f], :, :, k, f].transpose((0, 2, 1)), 0.2)
+                num_person[f] = p + 1
+                keypoint2d[0:num_person[f], 0:17, :, k, f] = pose2d[
+                    0:num_person[f], :, 0:17, k, f
+                ].transpose((0, 2, 1))
+                keypoint2d[0:num_person[f], 17:, :, k, f] = self.keypoint_generator(
+                    pose2d[0:num_person[f], :, :, k, f].transpose((0, 2, 1)), 0.2
+                )
                 heatmap = self.heatmap_generator(
-                    keypoint2d[0:num_person[f], :, :, k, f])
+                    keypoint2d[0:num_person[f], :, :, k, f]
+                )
 
-                img[:, :, :, k, f] = \
-                    cv2.warpAffine(
+                img[:, :, :, k, f] = cv2.warpAffine(
                     cv2.imread(str(subseq_hdImg[cam_id][f])),
                     trans,
                     (self.cfg.DATASET.INPUT_SIZE, self.cfg.DATASET.INPUT_SIZE),
-                    flags=cv2.INTER_LINEAR)
+                    flags=cv2.INTER_LINEAR,
+                )
                 hm[:, :, :, k, f] = heatmap
-        return {'heatmap': torch.from_numpy(hm), 'img': torch.from_numpy(img), 'keypoint2d': keypoint2d, 'num_person': num_person}
+        return {
+            "heatmap": torch.from_numpy(hm),
+            "img": torch.from_numpy(img),
+            "keypoint2d": keypoint2d,
+            "num_person": num_person,
+        }
 
     def readSkeletonFromPath(self, path):
         skeleton = []
         with open(path) as sfile:
             bframe = json.load(sfile)
-        for body in bframe['bodies']:
-            keypoint = np.array(body['joints19']).reshape(
-                (-1, 4)).transpose()
+        for body in bframe["bodies"]:
+            keypoint = np.array(body["joints19"]).reshape((-1, 4)).transpose()
             skeleton.append(self.mapKeypointsToCOCO(keypoint))
 
         return skeleton
@@ -257,7 +308,11 @@ class PanopticDataset(Dataset):
         return coco_keypoint
 
     def map3DkeypointsTo2d(self, keypoints3d, camera):
-        keypoints2d = panutils.projectPoints(keypoints3d[0:3, :],
-                                             camera['K'], camera['R'], camera['t'],
-                                             camera['distCoef'])
+        keypoints2d = panutils.projectPoints(
+            keypoints3d[0:3, :],
+            camera["K"],
+            camera["R"],
+            camera["t"],
+            camera["distCoef"],
+        )
         return keypoints2d
