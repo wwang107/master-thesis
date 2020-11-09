56b694f9f978ede0c64e158089d60121583a982b
wei <wei@pop-os.localdomain>
2020-11-09 19:30:31 +0100

    Modifiy dev

--- docker/dev.sh
+++ docker/dev.sh
@@ -1,5 +1,7 @@
 COCO_DATASET=$1
 CMU_DATASET=$2
+LIGHTENING_LOG_DIR=$3
+IMG_LOG_DIR=$3
 PWD=$(pwd)
 RUN_FOLDER="runs/"
 SRC_FOLDER="src/"
@@ -10,7 +12,8 @@ docker run --gpus all\
         -p 6006:6006\
         -v $COCO_DATASET:/workspace/dataset/coco\
         -v $CMU_DATASET:/workspace/dataset/panoptic\
-        -v $PWD/$RUN_FOLDER:/workspace/runs\
+        -v $LIGHTENING_LOG_DIR:/workspace/lightening_logs\
+        -v $IMG_LOG_DIR:/workspace/images\
         -v $PWD/$SRC_FOLDER:/workspace/src\
         -v /etc/localtime:/etc/localtime:ro\
         --rm -it weiwang/master-thesis:dev /bin/bash
