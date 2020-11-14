16b37962be3789d01efac36faa713dd094d7e9fb
wei <wei@pop-os.localdomain>
2020-11-14 20:44:03 +0100

    traing resnet encoder

--- src/models/resnet/model.py
+++ src/models/resnet/model.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torchvision.models as models
 
@@ -5,6 +6,7 @@ import torchvision.models as models
 class CustomizedResnet(nn.Module):
     def __init__(self, use_pretrained=True, fix_encoder_params=False):
         super().__init__()
+        self.fix_encoder_params = fix_encoder_params
         resnet18 = models.resnet18(pretrained=use_pretrained)
         self.encoder = nn.Sequential(*list(resnet18.children())[:-2])
         self.decoder = nn.Sequential(
@@ -17,17 +19,14 @@ class CustomizedResnet(nn.Module):
             nn.Conv2d(128, 55, 1)
         )
 
-        if fix_encoder_params:
-            for param in self.encoder.parameters():
-                param.requires_grad = False
-
     def forward(self, x):
         r"""
         Parmeter
         --------------------------
         x has a dimension of [N,channel,Height,Width]
         """
-        x = self.encoder(x)
+        with torch.set_grad_enabled(not self.fix_encoder_params):
+            x = self.encoder(x)
         return self.decoder(x)
 
     def deactive_batchnorm(self):
