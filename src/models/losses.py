949ea6572819f7fecc3090f0445028196d4fb3ec
wei <wwang107@gmail.com>
2020-12-21 03:15:40 +0100

    Switch to weighted loss

--- src/models/losses.py
+++ src/models/losses.py
@@ -2,14 +2,12 @@ import torch
 import torch.nn as nn
 
 
-class WeightedRegLoss(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-    def forward(self, pred, gt, mask):
+def forward(self, pred, gt, mask, fg_weight=1.0, bg_weight=0.1):
         assert pred.size() == gt.size()
         mask = mask[:, None, :, :].expand_as(pred)
-        loss = ((pred - gt)**2) * mask
+        weight = torch.where(
+            torch.eq(mask, 0.5), torch.tensor([fg_weight]).cuda(), torch.tensor([bg_weight]).cuda())
+        loss = ((pred - gt)**2) * weight
 
         loss = loss.mean() * 1000
         return loss
