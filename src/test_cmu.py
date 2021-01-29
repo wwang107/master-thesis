8224950030adabbd21ae260007ef0bbddbc472af
wei <wwang107@gmail.com>
2021-01-29 10:09:38 +0100

    typo

--- src/test_cmu.py
+++ src/test_cmu.py
@@ -92,7 +92,7 @@ def main(hparams):
     model = AggregateModel(resnet, Epipolar(debug=False), None, fuse_model, None,
                            weighted_mse_loss, in_channels, out_channels, 
                            train_input_heatmap_encoder=is_train_input_encoder, num_camera_can_see=cfg.DATASET.NUM_VIEW, num_frame_can_see=cfg.DATASET.NUM_FRAME_PER_SUBSEQ)
-    fusion_state_dict = torch.load('pretrain/fusion-model/with-resnet50/pretrain/late-fusion/epoch=2.ckpt')
+    fusion_state_dict = torch.load('pretrain/fusion-model/with-resnet50/late-fusion/epoch=2.ckpt')
     model.load_state_dict(fusion_state_dict['state_dict'])
     temporal_model = TemporalUnet(in_channels, out_channels, num_feature,9)
     print(temporal_model)
